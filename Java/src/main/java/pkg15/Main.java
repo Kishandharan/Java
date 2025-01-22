3a708ccd6efca97a1ee0021a362bdac41138a7e8
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-01-22 06:36:43 +0530

    Text Hopper

--- Java/src/main/java/pkg15/Main.java
+++ Java/src/main/java/pkg15/Main.java
@@ -1,8 +1,9 @@
-package pkg16;
+package pkg15;
 
 public class Main {
     public static void main(String[] args){
-        String[] arr1 = {"A","B","C","D"};
+        String str1 = "ABC";
+        String[] arr1 = str1.split("");
         String[] arr2 = new String[20];
         arr2[0]=arr1[0];
         arr2[1]=arr1[1];
